6ad38aac628934050f62e228da3f96fc75d02bab
Ricardo Venieris <rvenieris@gmail.com>
2022-10-20 10:55:58 -0300

    Initial Commit

---
+++ MovieAgendaSwiftUI/MovieAgendaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAgendaSwiftUIApp.swift
+//  MovieAgendaSwiftUI
+//
+//  Created by Ricardo Venieris on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieAgendaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
